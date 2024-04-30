99a78645b8f7dff665b2062ed8c79b6e8571169c
Alvin Pascasio <paz.alvin22@gmail.com>
2024-04-30 22:11:18 +0800

    init commit

--- src/components/weatherForeCastList.js
+++ src/components/weatherForeCastList.js
@@ -28,7 +28,7 @@ function WeatherForecastList(props) {
         <div class="w-[100%] flex justify-around gap-[10px] items-center" key={index + `_${item.dt_txt}`}>
             <img src={renderIcon(item.weather[0].icon)}  class="w-[100px] 2xs:w-[60px]"/>
             <p className="text-center">{dayjs(item.dt_txt).format('MM-DD h:mm')} <br />
-            <p class="text-center">{ item.weather[0].description }</p>        
+            <p class="text-center font-bold">{ item.weather[0].description }</p>        
             </p>
             <h4>{item?.main.temp ? item.main.temp : ""} { props.units === 'metric' ? '°' : '°F' }<br />
             </h4>
